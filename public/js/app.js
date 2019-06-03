26f17e41ceeef78ef7b8425b7aa84775db0ae2ee
Funsho Olaniyi <funsholaniyi@gmail.com>
2019-06-03 22:20:26 +0000

    Core requirements implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,28 @@
-window.addEventListener('load', () => {
-    const el = $('#app');
+const pageSize = 200;
+let startPage = 1;
+let endPage = 1;
+let pageNumber = 1;
+let all_data = JSON.parse(localStorage.getItem('__agric_data__'));
+let data = {};
+let sortHistory = [];
+const el = $('#app');
 
-    // Compile Handlebar Templates
-    const errorTemplate = Handlebars.compile($('#error-template').html());
-    const tableTemplate = Handlebars.compile($('#table-template').html());
+// Compile Handlebar Templates
+const errorTemplate = Handlebars.compile($('#error-template').html());
+const detailTemplate = Handlebars.compile($('#detail-template').html());
+const tableTemplate = Handlebars.compile($('#table-template').html());
+
+window.addEventListener('load', () => {
+
 
     // Instantiate api handler
     const api = axios.create({
-        baseURL: window.location.origin+'/api',
-        timeout: 5000,
+        baseURL: window.location.origin + '/api',
+        timeout: 10000,
     });
 
     const router = new Router({
-        mode: 'history',
-        page404: (path) => {
-            const html = errorTemplate({
-                color: 'yellow',
-                title: 'Error 404 - Page NOT Found!',
-                message: `The path '/${path}' does not exist on this site`,
-            });
-            el.html(html);
-        },
+        mode: 'history'
     });
 
     // Display Error Banner
@@ -37,53 +39,118 @@ window.addEventListener('load', () => {
         el.html(html);
         try {
             // Load Currency Rates
-            const response = await api.get('/');
-            data = response.data;
-            // localStorage.setItem('__agric_data__', JSON.stringify());
+
+            if (!all_data.length) {
+                const response = await api.get('/');
+                all_data = response.data;
+                localStorage.setItem('__agric_data__', JSON.stringify(all_data));
+            }
             // Display Rates Table
-            html = tableTemplate({data});
+            const selectedPageNumber = 1;
+            [startPage, endPage, pageNumber] = getPaginationValues(all_data.length, selectedPageNumber);
+
+            data = all_data.slice(startPage, endPage);
+            html = tableTemplate({data, pageNumber});
             el.html(html);
             $('.loading').removeClass('loading');
         } catch (error) {
             showError(error);
         }
     });
-    // Display Latest Currency Rates
-    // router.add('/page/', async () => {
-    //     // Display loader first
-    //     let html = tableTemplate();
-    //     el.html(html);
-    //     try {
-    //         // Load Currency Rates
-    //         data = JSON.parse(localStorage.getItem('__agric_data__'));
-    //         // Display Rates Table
-    //         html = tableTemplate({base, date, rates});
-    //         el.html(html);
-    //         $('.loading').removeClass('loading');
-    //     } catch (error) {
-    //         showError(error);
-    //     }
-    // });
 
 
     router.navigateTo(window.location.pathname);
 
-    // Highlight Active Menu on Load
-    const link = $(`a[href$='${window.location.pathname}']`);
-    link.addClass('active');
+});
 
-    $('a').on('click', (event) => {
-        // Block page load
+document.addEventListener('click', function (event) {
+
+    // If the clicked element doesn't have the right selector, bail
+    if (event.target.matches('.page-number')) {
+        // Don't follow the link
         event.preventDefault();
 
-        // Highlight Active Menu on Click
-        const target = $(event.target);
-        $('.item').removeClass('active');
-        target.addClass('active');
+        // Log the clicked element in the console
+        // console.log(event.target);
 
-        // Navigate to clicked url
-        const href = target.attr('href');
-        const path = href.substr(href.lastIndexOf('/'));
-        router.navigateTo(path);
-    });
+        const target = $(event.target);
+        // $('.item').removeClass('active');
+        // target.addClass('active');
+
+        // new page
+
+        const selectedPageNumber = target.attr('data-value');
+
+        [startPage, endPage, pageNumber] = getPaginationValues(all_data.length, selectedPageNumber);
+
+        data = all_data.slice(startPage, endPage);
+        let html = tableTemplate({data, pageNumber});
+        el.html(html);
+        $('.loading').removeClass('loading');
+    }
+
+    if (event.target.matches('.column-head')) {
+        // Don't follow the link
+        event.preventDefault();
+
+        const target = $(event.target);
+        // $('.item').removeClass('active');
+        // target.addClass('sorted');
+
+        // new page
+
+        const selectedColumn = target.attr('data-value');
+        const reverse = sortHistory.includes(selectedColumn);
+        !reverse ? sortHistory.push(selectedColumn) : sortHistory.splice( sortHistory.indexOf(selectedColumn), 1 );
+
+        data = data.sort(columnSorter(selectedColumn, reverse));
+        let html = tableTemplate({data, pageNumber});
+        el.html(html);
+        $('.loading').removeClass('loading');
+    }
+    if (event.target.matches('.row-item')) {
+        // Don't follow the link
+        event.preventDefault();
+
+        const target = $(event.target);
+        // $('.item').removeClass('active');
+        // target.addClass('sorted');
+
+        // new page
+
+        const selectedRow = target.attr('data-value');
+        const rowItem = data.find(x => x['sn'] === selectedRow);
+        let html = detailTemplate(rowItem);
+        el.html(html);
+        $('.loading').removeClass('loading');
+    }
+}, false);
+
+
+Handlebars.registerHelper('times', function (n, block) {
+    let sum = '';
+    for (let i = 1; i <= n; ++i)
+        sum += block.fn(i);
+    return sum;
 });
+
+
+columnSorter = function (column, reverse = false) {
+    return function (a, b) {
+        let val;
+        if (!isNaN(a[column]) && !isNaN(b[column])) {
+            val = parseFloat(a[column]) - parseFloat(b[column]);
+        } else {
+            val = a[column] > b[column] ? 1 : -1;
+        }
+        return reverse ? val * -1 : val;
+    }
+};
+
+getPaginationValues = function (totalRows, pageNumber) {
+    let startPage = pageSize * (pageNumber - 1);
+    let endPage = pageSize * (pageNumber);
+    let totalPageNumber = Math.ceil(totalRows / pageSize);
+
+    return [startPage, endPage, totalPageNumber]
+};
