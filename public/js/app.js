95773a331a0418fbb7e039310c824c7802ec4fb0
Funsho Olaniyi <funsholaniyi@gmail.com>
2019-06-03 16:44:38 +0000

    Loaded Data into View, Optimisation process begins

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,88 @@ window.addEventListener('load', () => {
     const el = $('#app');
 
     // Compile Handlebar Templates
+    const errorTemplate = Handlebars.compile($('#error-template').html());
     const tableTemplate = Handlebars.compile($('#table-template').html());
 
-    const html = tableTemplate();
-    el.html(html);
+    // Instantiate api handler
+    const api = axios.create({
+        baseURL: window.location.origin+'/api',
+        timeout: 5000,
+    });
+
+    const router = new Router({
+        mode: 'history',
+        page404: (path) => {
+            const html = errorTemplate({
+                color: 'yellow',
+                title: 'Error 404 - Page NOT Found!',
+                message: `The path '/${path}' does not exist on this site`,
+            });
+            el.html(html);
+        },
+    });
+
+    // Display Error Banner
+    const showError = (error) => {
+        const {title, message} = error.response.data;
+        const html = errorTemplate({color: 'red', title, message});
+        el.html(html);
+    };
+
+    // Display Latest Currency Rates
+    router.add('/', async () => {
+        // Display loader first
+        let html = tableTemplate();
+        el.html(html);
+        try {
+            // Load Currency Rates
+            const response = await api.get('/');
+            data = response.data;
+            // localStorage.setItem('__agric_data__', JSON.stringify());
+            // Display Rates Table
+            html = tableTemplate({data});
+            el.html(html);
+            $('.loading').removeClass('loading');
+        } catch (error) {
+            showError(error);
+        }
+    });
+    // Display Latest Currency Rates
+    // router.add('/page/', async () => {
+    //     // Display loader first
+    //     let html = tableTemplate();
+    //     el.html(html);
+    //     try {
+    //         // Load Currency Rates
+    //         data = JSON.parse(localStorage.getItem('__agric_data__'));
+    //         // Display Rates Table
+    //         html = tableTemplate({base, date, rates});
+    //         el.html(html);
+    //         $('.loading').removeClass('loading');
+    //     } catch (error) {
+    //         showError(error);
+    //     }
+    // });
+
+
+    router.navigateTo(window.location.pathname);
+
+    // Highlight Active Menu on Load
+    const link = $(`a[href$='${window.location.pathname}']`);
+    link.addClass('active');
+
+    $('a').on('click', (event) => {
+        // Block page load
+        event.preventDefault();
+
+        // Highlight Active Menu on Click
+        const target = $(event.target);
+        $('.item').removeClass('active');
+        target.addClass('active');
+
+        // Navigate to clicked url
+        const href = target.attr('href');
+        const path = href.substr(href.lastIndexOf('/'));
+        router.navigateTo(path);
+    });
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-require('dotenv').config(); // read .env files
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const {getAgricultureData} = require('./lib/agric-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,10 +9,48 @@ const port = process.env.PORT || 3000;
 // Set public folder as root
 app.use(express.static('public'));
 
-// Allow front-end access to node_modules folder
+// Parse POST data as URL encoded data
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+// Parse POST data as JSON
+app.use(bodyParser.json());
+
+// Provide access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
-// Listen for HTTP requests on port 3000
+
+const errorHandler = (err, req, res) => {
+    if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        res.status(403).send({title: 'Server responded with an error', message: err.message});
+    } else if (err.request) {
+        // The request was made but no response was received
+        res.status(503).send({title: 'Unable to communicate with server', message: err.message});
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        res.status(500).send({title: 'An unexpected error occurred', message: err.message});
+    }
+};
+
+// Fetch Latest Currency Rates
+app.get('/api/', async (req, res) => {
+    try {
+        const data = await getAgricultureData();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    } catch (error) {
+        errorHandler(error, req, res);
+    }
+});
+
+
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log('listening on %d', port);
 });
